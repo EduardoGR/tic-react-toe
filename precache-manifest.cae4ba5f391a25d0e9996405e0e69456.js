5ea216ffa3a35f07dd794ee4b2d6082292a47b6e
egarcia <egarcia@tuenti.com>
2019-01-26 23:30:49 -0200

    Updates

---
+++ precache-manifest.cae4ba5f391a25d0e9996405e0e69456.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36dd5e4cf45fc7e1680",
+    "url": "/tic-react-toe/static/js/runtime~main.e36dd5e4.js"
+  },
+  {
+    "revision": "319ebf45d1f851e6f510",
+    "url": "/tic-react-toe/static/js/main.319ebf45.chunk.js"
+  },
+  {
+    "revision": "722cf89b0f3d62f5451b",
+    "url": "/tic-react-toe/static/js/1.722cf89b.chunk.js"
+  },
+  {
+    "revision": "112628c0636c21939e392aa8e93cc57e",
+    "url": "/tic-react-toe/index.html"
+  }
+];
